b70bd55a0041867923746fae54ade7809dae51ff
Paolo Bozzo <pagolo.bozzo@gmail.com>
2014-12-14 14:13:07 +0100

    classes admin interfaces

---
+++ administrator/controllers/classi.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @version     0.0.1
+ * @package     com_receivements
+ * @copyright   Copyright (C) 2014. Tutti i diritti riservati.
+ * @license     GNU General Public License versione 2 o successiva; vedi LICENSE.txt
+ * @author      Paolo Bozzo <info@dbfweb.com> - http://dbfweb.com
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+jimport('joomla.application.component.controlleradmin');
+
+/**
+ * Sedi list controller class.
+ */
+class ReceivementsControllerClassi extends JControllerAdmin
+{
+	/**
+	 * Proxy for getModel.
+	 * @since	1.6
+	 */
+	public function getModel($name = 'classe', $prefix = 'ReceivementsModel')
+	{
+		$model = parent::getModel($name, $prefix, array('ignore_request' => true));
+		return $model;
+	}
+    
+    
+	/**
+	 * Method to save the submitted ordering values for records via AJAX.
+	 *
+	 * @return  void
+	 *
+	 * @since   3.0
+	 */
+	public function saveOrderAjax()
+	{
+		// Get the input
+		$input = JFactory::getApplication()->input;
+		$pks = $input->post->get('cid', array(), 'array');
+		$order = $input->post->get('order', array(), 'array');
+
+		// Sanitize the input
+		JArrayHelper::toInteger($pks);
+		JArrayHelper::toInteger($order);
+
+		// Get the model
+		$model = $this->getModel();
+
+		// Save the ordering
+		$return = $model->saveorder($pks, $order);
+
+		if ($return)
+		{
+			echo "1";
+		}
+
+		// Close the application
+		JFactory::getApplication()->close();
+	}
+    
+    
+    
+}
