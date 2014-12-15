11ae2038788aa88f6c7be767791c0edccda09c90
Paolo Bozzo <pagolo.bozzo@gmail.com>
2014-12-16 00:12:45 +0100

    receivements query updated

--- site/models/ricevimenti.php
+++ site/models/ricevimenti.php
@@ -9,9 +9,11 @@ class ReceivementsModelRicevimenti extends JModelList
     {
         $db = JFactory::getDBO();
         $query = $db->getQuery(true);
-        $query->select('id_docente,u.name,classi,giorno');
-        $query->from('#__receivements_ore');
+        $query->select('o.id,u.name,c.materie,classi,giorno,inizio');
+        $query->from('#__receivements_ore AS o');
         $query->join('LEFT', $db->quoteName('#__users', 'u') . ' ON (' . $db->quoteName('id_docente') . ' = ' . $db->quoteName('u.id') . ')');
+        $query->join('LEFT', $db->quoteName('#__receivements_cattedre', 'c') . ' ON (' . $db->quoteName('cattedra') . ' = ' . $db->quoteName('c.id') . ')');
+        $query->where('o.attiva <> ' . $db->Quote('0'));
         return $query;
     }
 }
