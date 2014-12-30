740734aae8563f7b25b64f1f68f0b13ef9506114
Paolo Bozzo <pagolo.bozzo@gmail.com>
2014-12-30 18:02:50 +0100

    rules #1

---
+++ site/models/rules/ricevimenti.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ *
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * Form Rule class for the Joomla Platform.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Form
+ * @since       11.1
+ */
+class JFormRuleRicevimenti extends JFormRule
+{
+	/**
+	 * The regular expression to use in testing a form field value.
+	 *
+	 * @var    string
+	 * @since  11.1
+	 */
+	protected $regex = '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$';
+	
+
+	/**
+	 * Method to test the email address and optionally check for uniqueness.
+	 *
+	 * @param   SimpleXMLElement  &$element  The SimpleXMLElement object representing the <field /> tag for the form field object.
+	 * @param   mixed             $value     The form field value to validate.
+	 * @param   string            $group     The field name group control value. This acts as as an array container for the field.
+	 *                                       For example if the field has name="foo" and the group value is set to "bar" then the
+	 *                                       full field name would end up being "bar[foo]".
+	 * @param   JRegistry         &$input    An optional JRegistry object with the entire data set to validate against the entire form.
+	 * @param   object            &$form     The form object for which the field is being tested.
+	 *
+	 * @return  boolean  True if the value is valid, false otherwise.
+	 *
+	 * @since   11.1
+	 * @throws  JException on invalid rule.
+	 */
+	public function test(&$element, $value, $group = null, &$input = null, &$form = null)
+	{
+		// If the field is empty and not required, the field is valid.
+		$required = ((string) $element['required'] == 'true' || (string) $element['required'] == 'required');
+		if (!$required && empty($value))
+		{
+			return true;
+		}
+
+		// Test the value against the regular expression.
+		//if (!parent::test($element, $value, $group, $input, $form))
+		//{
+		//	return false;
+		//}
+                echo "value=$value<br>";
+                print_r($input);
+
+		return true;
+	}
+}
