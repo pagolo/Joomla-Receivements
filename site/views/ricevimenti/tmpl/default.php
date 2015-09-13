5d4b2be84c3221acee9c479ca9716c1504a24c35
paolo <pagolo.bozzo@gmail.com>
2015-09-13 18:29:00 +0200

    debugging

--- site/views/ricevimenti/tmpl/default.php
+++ site/views/ricevimenti/tmpl/default.php
@@ -21,7 +21,7 @@ $showClass = $class === '*' || empty($class);
 
 <form id="primo" action="<?php echo 'index.php?option=com_receivements&amp;view=ricevimenti' ?>" method="post">
 	<div>
-<?php if (!(empty(ReceivementsFrontendHelper::getSchoolsGroup()))) : ?>
+<?php if (ReceivementsFrontendHelper::getSchoolsGroup()) : ?>
 			<select name="filter_school" class="inputbox school-select" onchange="this.form.submit();">
 				<option value="*"><?php echo JText::_('COM_RECEIVEMENTS_ALL_SCHOOLS');?></option>
 				<?php echo JHtml::_('select.options', ReceivementsFrontendHelper::getSchoolOptions(), 'value', 'text', $this->state->get('filter.scuola'));?>
